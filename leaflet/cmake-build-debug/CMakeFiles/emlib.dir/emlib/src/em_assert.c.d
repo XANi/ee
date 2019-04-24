0850faf5610eb21f31f94cd2a36c70740ed5561c
Mariusz Gronczewski <xani666@gmail.com>
2019-04-24 23:35:49 +0200

    trying to make cmake work

--- leaflet/cmake-build-debug/CMakeFiles/emlib.dir/emlib/src/em_assert.c.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/emlib.dir/emlib/src/em_assert.c.obj: \
- /home/xani/src/my/ee/leaflet/emlib/src/em_assert.c \
- /home/xani/src/my/ee/leaflet/emlib/inc/em_assert.h
-
-/home/xani/src/my/ee/leaflet/emlib/inc/em_assert.h:
