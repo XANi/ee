9a0c83fbb82594f845164d88dca7cb4019d4dbb8
Mariusz Gronczewski <xani666@gmail.com>
2020-09-06 21:15:46 +0200

    blinky on bluepill

---
+++ sid/cube/CMakeFiles/cube.elf.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/xani/src/my/ee/sid/cube/Core/Startup/startup_stm32f103c8tx.s" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Core/Startup/startup_stm32f103c8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "Core/Inc"
+  "Drivers/STM32F1xx_HAL_Driver/Inc"
+  "Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/xani/src/my/ee/sid/cube/Core/Src/main.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Core/Src/main.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Core/Src/stm32f1xx_hal_msp.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Core/Src/stm32f1xx_hal_timebase_tim.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Core/Src/stm32f1xx_hal_timebase_tim.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Core/Src/stm32f1xx_it.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Core/Src/system_stm32f1xx.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "/home/xani/src/my/ee/sid/cube/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "/home/xani/src/my/ee/sid/cube/CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "Core/Inc"
+  "Drivers/STM32F1xx_HAL_Driver/Inc"
+  "Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sid/cube/CMakeFiles/cube.elf.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cube.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/cube.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/cube.elf.dir/Core/Src/stm32f1xx_hal_timebase_tim.c.obj"
+  "CMakeFiles/cube.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/cube.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/cube.elf.dir/Core/Startup/startup_stm32f103c8tx.s.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/cube.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "cube.elf"
+  "cube.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/cube.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
